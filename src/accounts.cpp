ffafe8d6db408855ac858a205855cad10ca3c23e
TMSolodinov24 <TMSolodinov24@codingburgas.bg>
2026-03-15 14:32:43 +0200

    Add average user success

--- src/accounts.cpp
+++ src/accounts.cpp
@@ -17,52 +17,53 @@ bool userExists(string username) {
     return f.good();
 }
 
-// 0 - Success, 1 - Error
+// Returns: 0 - Success, 1 - Error
 bool registerUser(string username, string password) {
-    if (userExists(username)) return false;
+    if (userExists(username)) return 1;
 
     ofstream outFile(getFileName(username));
-    if (!outFile.is_open()) return false;
+    if (!outFile.is_open()) return 1;
 
     password = hash_sha256(password.c_str());
     outFile << password << "\n";
     outFile << 1 << "\n";
+    outFile << 0 << "\n";
     outFile.close();
-    return true;
+    return 0;
 }
 
 // Returns: 0 - Success, 1 - Error
-bool loginUser(string username, string inputPassword, int& task) {
+bool loginUser(string username, string inputPassword, int& task, int& avg) {
     ifstream inFile(getFileName(username));
-    if (!inFile.is_open()) return false;
+    if (!inFile.is_open()) return 1;
 
     string savedPassword;
     if (!getline(inFile, savedPassword)) {
         inFile.close();
-        return false;
+        return 1;
     }
 
     inputPassword = hash_sha256(inputPassword.c_str());
     if (inputPassword != savedPassword) {
         inFile.close();
-        return false;
+        return 1;
     }
 
-    if (!(inFile >> task)) {
-        task = 1;
-    }
+    if (!(inFile >> task)) task = 1;
+    if (!(inFile >> avg)) avg = 0;
 
     inFile.close();
-    return true;
+    return 0;
 }
 
 // Returns: 0 - Success, 1 - Error
-bool updateInfo(string username, string password, int task) {
+bool updateInfo(string username, string password, int task, int avg) {
     ofstream outFile(getFileName(username));
-    if (!outFile.is_open()) return false;
+    if (!outFile.is_open()) return 1;
 
     outFile << password << "\n";
     outFile << task << "\n";
+    outFile << avg << "\n";
     outFile.close();
-    return true;
+    return 0;
 }
